ed881b7c3e937844bdf2764a4ae3a5a58f6cb8a9
Yuri Lee <yrlee@mycreatz.com>
2024-05-08 18:06:52 +0900

    fix lint

--- grpc_node/client.ts
+++ grpc_node/client.ts
@@ -1,33 +1,32 @@
-import { ServiceError, credentials} from '@grpc/grpc-js';
-import { PeopleServiceClient, PeopleData, PeopleId, PeopleIdList } from './proto/people';
+import { ServiceError, credentials } from '@grpc/grpc-js';
+import {
+  PeopleServiceClient,
+  PeopleData,
+  PeopleId,
+  PeopleIdList,
+} from './proto/people';
 
 const peopleId: PeopleId = {
-    id: 0
+  id: 0,
 };
 
 const peopleIdList: PeopleIdList = {
-  id: [0, 1, 2]
+  id: [0, 1, 2],
 };
 
 const client = new PeopleServiceClient(
   'localhost:8080',
-  credentials.createInsecure()
+  credentials.createInsecure(),
 );
 
-client.getData(
-  peopleId,
-  (err: ServiceError | null, response: PeopleData) => {
-    console.log(`get unary data peopleId: ${peopleId.id}`);
-    console.log(JSON.stringify(response));
-    }
-);
+client.getData(peopleId, (err: ServiceError | null, response: PeopleData) => {
+  console.log(`get unary data peopleId: ${peopleId.id}`);
+  console.log(JSON.stringify(response));
+});
 
-
-const peopleStream = client.getListData(
-  peopleIdList
-);
+const peopleStream = client.getListData(peopleIdList);
 peopleStream.on('metadata', () => {
-console.log('get stream data peopleIdList: [0,1,2]');
+  console.log('get stream data peopleIdList: [0,1,2]');
 });
 
 peopleStream.on('data', (data: PeopleData) => {
@@ -35,6 +34,4 @@ peopleStream.on('data', (data: PeopleData) => {
 });
 peopleStream.on('end', () => {
   console.log('stream end');
-}
-);
-
+});
--- grpc_node/server.ts
+++ grpc_node/server.ts
@@ -1,33 +1,44 @@
-import { Server, ServerCredentials, ServerUnaryCall, sendUnaryData,ServerWritableStream  } from '@grpc/grpc-js';
+import {
+  Server,
+  ServerCredentials,
+  ServerUnaryCall,
+  sendUnaryData,
+  ServerWritableStream,
+} from '@grpc/grpc-js';
 import { PeopleServiceService } from './proto/people';
 import { PeopleId, PeopleData, PeopleIdList } from './proto/people';
 
-const peopleDataBase:PeopleData[] = [
-    { name: 'Otonose Kanade', address: "seoul, korea",  age: 20 },
-    { name: 'Ichijou Ririka', address: "tokyo, japan",  age: 23 },
-    { name: 'Hiodoshi Ao', address: "tokyo, japan",  age: 22 },
-    { name: 'Juufuutei Raden', address: "tokyo, japan",  age: 22 },
-    { name: 'Todoroki Hajime', address: "tokyo, japan",  age: 21 },
-]
+const peopleDataBase: PeopleData[] = [
+  { name: 'Otonose Kanade', address: 'seoul, korea', age: 20 },
+  { name: 'Ichijou Ririka', address: 'tokyo, japan', age: 23 },
+  { name: 'Hiodoshi Ao', address: 'tokyo, japan', age: 22 },
+  { name: 'Juufuutei Raden', address: 'tokyo, japan', age: 22 },
+  { name: 'Todoroki Hajime', address: 'tokyo, japan', age: 21 },
+];
 
-const GetData = (call: ServerUnaryCall<PeopleId, PeopleData>, callback: sendUnaryData<PeopleData>) => {
-    let id = call.request.id;
-    console.log(id);
-    const data:PeopleData = peopleDataBase[id];
-    callback(null, data);
-}
+const GetData = (
+  call: ServerUnaryCall<PeopleId, PeopleData>,
+  callback: sendUnaryData<PeopleData>,
+) => {
+  const id = call.request.id;
+  console.log(id);
+  const data: PeopleData = peopleDataBase[id];
+  callback(null, data);
+};
 const GetListData = (call: ServerWritableStream<PeopleIdList, PeopleData>) => {
-    // console.log(call.request.id);
-    call.request.id.forEach(element => {
-        console.log(`send data: ${element}`);
-        call.write(peopleDataBase[element]);
-    });
-    call.end();
-}
+  // console.log(call.request.id);
+  call.request.id.forEach((element) => {
+    console.log(`send data: ${element}`);
+    call.write(peopleDataBase[element]);
+  });
+  call.end();
+};
 
-  
 const server = new Server();
-server.addService(PeopleServiceService, { getData: GetData, getListData: GetListData}); 
-server.bindAsync('localhost:8080', ServerCredentials.createInsecure(), () => {console.log("server start");});
-
-
+server.addService(PeopleServiceService, {
+  getData: GetData,
+  getListData: GetListData,
+});
+server.bindAsync('localhost:8080', ServerCredentials.createInsecure(), () => {
+  console.log('server start');
+});
